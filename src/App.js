46bf01b9e0507c9156244fef22d2eb8fc5c8e09d
Cobb <cobb@thecobralounge.com>
2022-05-19 11:56:42 -0700

    Added footer.js and contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import Currency from "./Components/Currency.js";
 import Nav from "./Components/Nav.js";
 import This from "./Components/This.js";
+import Footer from "./Components/Footer.js";
+import SignUp from "./Components/SignUp.js";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
       <Routes>
         <Route path="/about" element={<Currency />}></Route>
         <Route path="/this" element={<This />}></Route>
+        <Route path="/SignUp" element={<SignUp />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div class="bijon">
+      <h4 id="b">Join over 30 Million People Who Use Currency!</h4>
+      <p>(Just Kidding)</p>
+      <button class="btn btn-outline-success" type="link">
+        <Link to="/SignUp">Sign up For Free.</Link>
+      </button>
+    </div>
+  );
+};
+
+export default Footer;
+
+{
+  /* <button class="btn btn-outline-success" type="link">
+  <Link className="crncy" to="/about">
+    Currency
+  </Link>
+</button>; */
+}
